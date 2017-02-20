ba6e98aff252d5b57e9911a514955a90c8786ac7
Henry McC <henrymcc@gmail.com>
2017-02-20 22:09:32 +0200

    Setting up the booksReducer Code within reducers folder

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+function() {
+  return [
+    { title: 'Javascript: the good parts'},
+    { title: 'Harry Potter'},
+    { title: 'The Dark Tower'},
+    { title: 'Book number 4 - React - Redux - Huh'}
+  ]
+}
